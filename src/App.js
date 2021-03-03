b45edfed62a005e9d6635c50b2d6d6c339d51aab
GEORGE BURTON <georgeburton@live.co.uk>
2021-03-03 03:06:43 +0000

    Added heading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useState, useEffect} from 'react';
 import ImageCard from './components/ImageCard';
 import ImageSearch from './components/ImageSearch';
+import {ReactComponent as Logo} from './assets/images/Pixabay-logo.svg'
+
+
 
 function App() {
 
@@ -20,6 +23,15 @@ function App() {
 
   return (
     <div className="container mx-auto">
+
+    <div className="flex items-center">
+      <Logo className="mx-auto mt-8"/>
+     
+    </div>
+      
+      
+    
+
       <ImageSearch searchText={(text) => setTerm(text)}/>
 
       {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './assets/main.css';
 import App from './App';
 
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
